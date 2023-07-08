9ebec1d1b03c55d98bb835004cd80f6cb542463d
isaacasher97 <isaac.asher97@gmail.com>
2023-07-08 12:45:12 -0400

    set up index.ejs and it's route + tested that it works and displays the object data

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config(); // bring in our .env vars
 const express = require('express'); // web framework for node
 const morgan = require('morgan'); // logger for node
 const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
+const Fighter = require('./models/fighter');
 
 // express application
 const app = express();
@@ -14,9 +15,10 @@ app.use(express.static('public')); // serve static files from public folder
 
 // Routes
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    })
+app.get('/fighter', async (req, res) => {
+    const allFighters = await Fighter.find({})
+    res.render('fighters/index.ejs', {fighters: allFighters})
+})
 
 
 // Listen 
