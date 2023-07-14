110605c620749e3ab76051b0125e1f66715ccd71
isaacasher97 <isaac.asher97@gmail.com>
2023-07-14 02:14:23 -0400

    added secret variable + middleware for session

--- server.js
+++ server.js
@@ -2,20 +2,29 @@
 require('dotenv').config(); // bring in our .env vars
 const express = require('express'); // web framework for node
 const morgan = require('morgan'); // logger for node
-const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
 const FighterRouter = require('./controllers/fighter')
-const UserRouter = require('./controllers/user')
+const UserRouter = require('./controllers/user');
+const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 // express application
 const app = express();
 
 // middleware
 app.use(morgan('dev')); // logging
+app.use(express.static('public')); // serve static files from public folder
 app.use(express.urlencoded()); // allows the req.body to be read from the form
 app.use(methodOverride('_method')); // override with POST having ?_method=DELETE or ?_method=PUT
-app.use(express.static('public')); // serve static files from public folder
+app.use(session({
+    secret: process.env.SECRET,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL}),
+    saveUninitialized: true,
+    resave: false,
+}));
 app.use("/fighter", FighterRouter);
 app.use("/user", UserRouter);
+
 
 //redirect "/" route on deployed site to /fighter
 
