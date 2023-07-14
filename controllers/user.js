5f712af83b4fec71b52bed93675a71bd5f4f761c
isaacasher97 <isaac.asher97@gmail.com>
2023-07-14 00:10:39 -0400

    fixed /signup .post route + tested functionality

--- controllers/user.js
+++ controllers/user.js
@@ -9,8 +9,10 @@ router.get('/signup', (req, res) => {
     res.render('users/signup.ejs')
 })
 
-router.post('/signup', (req, res) => {
-    res.send('signup')
+router.post('/signup', async (req, res) => {
+   req.body.password = await bcrypt.hash(req.body.password, await bcrypt.genSalt(10)) // generates a random string with 10 charachters and encrypt the password using that random string
+   await User.create(req.body);
+   res.redirect('/user/login');
 })
 
 router.get('/login', (req, res) => {
